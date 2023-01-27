aa2611bd23a3f58db03286ca82cca7d89978d5f4
MicheleRossetti02 <michelerossetti07@gmail.com>
2023-01-27 01:33:03 +0100

    fix again Leadcontroller

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,43 +3,40 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Mail\NewContact;
-use App\Models\Lead;
 use Illuminate\Http\Request;
+use App\Models\Lead;
+use App\Mail\NewContact;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
-
 
 class LeadController extends Controller
 {
     public function store(Request $request)
     {
-        # code...
-$data= $request->all();
+        $data = $request->all();
 
-// dd($data);
-$new_lead=new Lead();
-$new_lead->fill($data);
-$new_lead->save();
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required'
+        ]);
 
-$validator = Validator::make($data,[
-    'name'=>'required',
-    'email'=>'required',
-    'message'=>'required',
+        if ($validator->fails()) {
 
-]);
-if($validator->fails()){
-    return response()->json([
-        'success'=> false,
-        'errors'=>$validator->errors(),
-    ]);
-}
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
 
-Mail::to('michelerossetti02@gmail.com')->send(new NewContact($new_lead));
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
 
-return response()->json([
-    'success'=>true
-]);
+        Mail::to('admin@laravel.it')->send(new NewContact($new_lead));
+
+        return response()->json([
+            'success' => true
+        ]);
     }
-
 }
